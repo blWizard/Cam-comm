9f7a89832bb2d6c43a6707f239d0d9aea4df4d57
blWizard <mtecuela09@gmail.com>
2025-08-30 14:00:37 -0300

    Solved emmiter issues

--- emmiter/emmiter.py
+++ emmiter/emmiter.py
@@ -1,10 +1,12 @@
 # Untitled - By: mtala - Fri Aug 29 2025
 
-from machine import UART
+# from machine import UART
+# import ustruct
 import sensor
 import time
+import pyb
 
-uart = UART(1, 9600)
+# uart = UART(1, 9600)
 # uart.init(9600) # Baudios
 
 sensor.reset()
@@ -13,11 +15,20 @@ sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time=2000)
 
 clock = time.clock()
+led = pyb.LED(2)
+uart = pyb.UART(1, 9600)
 
 while True:
-    clock.tick()
+    # clock.tick()
+    led.toggle()
+
     img = sensor.snapshot()
-    # print(clock.fps())
-    data = bytes("Hola\n", 'utf-8')
+    # # print(clock.fps())
+    # data = ustruct.pack('i', -2)
+    # data = bytes('-2\n')
+    offset = -1.7698
+    data = str(offset) + '\n'
+    # print(data)
     uart.write(data)
+    time.sleep(1)
     # print(data)
